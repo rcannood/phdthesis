5fa33cd6ea22bafe2caabe33a5e924fcc5a6dfb8
Robrecht Cannoodt <rcannood@gmail.com>
2019-12-11 12:22:44 +0100

    update

---
+++ scripts/color_pages.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+col <- system("gs -o - -sDEVICE=inkcov 'boek_v2.0_20191211.pdf'", intern = TRUE)
+
+pagix <- grep("^Page [0-9]*$", col)
+colix <- grep("CMYK OK$", col)
+
+pages <- col[pagix] %>% gsub("Page ", "", .) %>% as.integer()
+colors <- col[colix] %>% gsub("^ *(.*) *CMYK OK$", "\\1", .) %>% strsplit("  *") %>% map(as.numeric) %>% do.call(rbind, .)
+colnames(colors) <- c("C", "M", "Y", "K")
+
+coldf <- data.frame(
+  page = pages,
+  colors
+)
+
+
+colpag <- coldf %>% filter(C > 0 | M > 0 | Y > 0)
+pages <- colpag$page
+diff(pages)
+
+
+length(pages)
+nrow(coldf) - length(pages)
